782dbd0962d200a6492e549a9e526d7a2ed79b5e
benpearo <benpearo@gmail.com>
2019-07-27 03:30:39 -0400

    need to finish response parsing

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,8 @@
 import * as functions from 'firebase-functions';
+import * as cors from 'cors';
+import * as cheerio from 'cheerio';
+import * as getUrls from 'get-urls';
+import fetch from 'node-fetch';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -6,3 +10,51 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+console.log(functions);
+console.log(cors);
+console.log(cheerio);
+console.log(getUrls);
+console.log(fetch);
+
+const scrapeHeader = (text: string) => {
+
+    // extracts urls from string
+    const urls = Array.from(getUrls(text));
+
+    // create list of requests with url and title from html
+    const requests = urls.map(async url => {
+
+        // get
+        const res = await fetch(url);
+
+        const html = await res.text();
+
+        const $ = cheerio.load(html);
+
+        return {
+            url,
+            title: $('h1').first().text()
+        }
+
+    });
+
+    // send all resolve then return all?
+    return Promise.all(requests);
+
+}
+
+export const scraper = functions.https.onRequest(async (request, response) => {
+
+    console.log(request.body);
+
+    try {
+        const body = JSON.parse(request.body);
+        const data = await scrapeHeader(body.text);
+        response.send(data);
+    }
+    catch (err) {
+        console.error("Something went wrong");
+        console.log(err);
+        response.sendStatus(500)
+    }
+});
